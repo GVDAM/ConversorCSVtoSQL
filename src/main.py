626937888342a37e3c53c5b56ceb9d0385d57403
Rodolfo Teles <rodolfojeronimoteles@gmail.com>
2021-04-13 08:20:02 -0300

    Adicionando comentários e docstring na main.py

--- src/main.py
+++ src/main.py
@@ -3,12 +3,19 @@ from fastapi import FastAPI, File, UploadFile
 import pandas as pd
 from io import BytesIO
 
+# Instancia um objeto para iniciar a aplicação
 app = FastAPI()
 
 @app.post("/files/")
 async def create_file(file: UploadFile = File(...)):
+    """
+	Função assíncrona que recebe um arquivo.
+    """
+    # O arquivo uploaded é lido como um 
     contents = await file.read()
+    # O conteúdo do arquivo é convertido em Bytes
     arquivo = BytesIO(contents)
+    # O arquivo convertido é transformado em DataFrame
     df = pd.read_csv(arquivo)
     print(type(contents))
     print(type(arquivo))
